d3992bbc5b91ba1fe179b68f1e258eedee8cd0f1
Andrij-Maksymyshyn <84930890+Andrij-Maksymyshyn@users.noreply.github.com>
2022-01-26 00:39:26 +0200

    cor

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,58 +1,73 @@
 import { Component } from 'react';
-import { getImage } from '../../getImage/getImage';
+import axios from 'axios';
 import { Toaster, toast } from 'react-hot-toast';
 import Searchbar from '../Searchbar';
 import ImageGallery from '../ImageGallery';
 import Button from '../Button';
 import { Container } from './App.styled';
 
+const KEY = '24382748-1dfb63c81149146d5ea200f75';
+const BASE_URL = 'https://pixabay.com/api';
+
 class App extends Component {
   state = {
     searchValue: '',
     images: [],
     page: 1,
-    perPage: 12,
-    totalHits: 0,
     endPage: 0,
+    perPage: 12,
   };
 
   addSearchValue = formData => {
     this.setState({
       searchValue: formData,
+      page: 1,
+      images: [],
     });
   };
 
-  componentDidUpdate(_, prevStates) {
-    const { searchValue, page, perPage } = this.state;
-    if (prevStates.searchValue !== searchValue) {
-      return getImage(searchValue, page === 1).then(data => {
-        if (data.hits.length === 0) {
-          return toast.error(
-            'Sorry, there is no pictures. Try another request...',
-          );
-        }
-        this.setState({
-          images: [...data.hits],
-          page: 1,
-          totalHits: data.totalHits,
-          endPage: perPage + 1,
-        });
-      });
+  async componentDidUpdate(_, prevState) {
+    const { searchValue, images, page, perPage, endPage } = this.state;
+    if (prevState.searchValue !== searchValue || prevState.page !== page) {
+      try {
+        await axios
+          .get(
+            `${BASE_URL}/?q=${searchValue}&page=${page}&key=${KEY}&image_type=photo&orientation=horizontal&per_page=${perPage}
+`,
+          )
+          .then(data => {
+            const {
+              data: { hits, totalHits },
+            } = data;
+
+            if (hits.length === 0) {
+              return toast.error(
+                'Sorry, there is no pictures. Try another request...',
+              );
+            }
+
+            this.setState(prevState => {
+              return {
+                images: [...prevState.images, ...hits],
+                endPage: totalHits - page * perPage,
+              };
+            });
+
+            if (images.length > 0 && endPage < perPage) {
+              return toast.error('Pictures are finished.Try new request');
+            }
+          });
+      } catch (error) {
+        toast.error('Whoops, something went wrong:', error);
+      }
     }
   }
 
   handleClick = () => {
-    const { searchValue, page } = this.state;
-
-    getImage(searchValue, page + 1).then(data => {
-      this.setState(prevState => {
-        return {
-          images: [...prevState.images, ...data.hits],
-          page: page + 1,
-          totalHits: data.totalHits,
-          endPage: data.totalHits - page * 12,
-        };
-      });
+    this.setState(prevState => {
+      return {
+        page: prevState.page + 1,
+      };
     });
   };
 
@@ -60,13 +75,15 @@ class App extends Component {
     const { images, endPage, perPage } = this.state;
     return (
       <Container>
-        <Searchbar onSubmit={this.addSearchValue} />
         <Toaster position="top-right" />
+        <Searchbar onSubmit={this.addSearchValue} />
         {images.length > 0 && <ImageGallery prop={this.state.images} />}
-        {endPage > perPage && <Button buttonP={this.handleClick} />}
+        {endPage > perPage / 2 && <Button buttonP={this.handleClick} />}
       </Container>
     );
   }
 }
 
 export default App;
+
+// npm install spinners-react
